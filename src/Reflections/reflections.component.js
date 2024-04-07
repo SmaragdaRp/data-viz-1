061f6d20a7768cf0f3de124e5c4980a813c79f2e
sreppa <sreppa@di.uoa.gr>
2024-04-07 22:22:04 +0300

    added papers dividers

--- src/Reflections/reflections.component.js
+++ src/Reflections/reflections.component.js
@@ -1,9 +1,19 @@
 import React from "react";
+import my_photo from "../resources/photo_me.jpg";
 
 export default function Reflections() {
     return (
-        <div>
-            Skata
+        <div className=' flex flex-row h-full'>
+            <div className=" max-w-5xl py-6 px-10 text-left">
+                <h2 className="text-4xl font-bold">
+                    Perception! Immersion! Empowerment!
+                </h2>
+                <h2 className="text-2xl font-bold">
+                    Superpowers as Inspiration for Visualization
+                </h2>
+
+            </div>
+
         </div>
     )
 }
